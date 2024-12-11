f9074781ea4f8da5b3f5b70e8f63a80ac59e7376
Eduardofbarreto <seuemail@example.com>
2024-12-11 10:27:54 -0300

    passo 3

--- calc.cpp
+++ calc.cpp
@@ -4,9 +4,13 @@
         return num1 + num2;
     }
 
+    int subt(int num1, int num2){
+        return num1 - num2;
+    }
+
 int	main(){
     
-    int num1, num2, resultadoSoma;
+    int num1, num2, resultadoSoma, resultadoSubtracao;
 
     
     std::cout<<"Bem-vindos a calculadora de Edu F. Barreto!"<<std::endl;
@@ -19,10 +23,12 @@ int	main(){
 
     //Blocos de cálculos
     resultadoSoma = soma(num1, num2);
+    resultadoSubtracao = subt(num1, num2);
     
     //Blocos de resultados para o usuário
     std::cout<<"\n";
     std::cout<<"O resultado da soma e: "<<resultadoSoma<<std::endl;
+    std::cout<<"O resultado da subtracao e: "<<resultadoSubtracao<<std::endl;
     std::cout<<"\n"<<std::endl;
 
     return 0;
